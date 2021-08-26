652da1c6ca52c26d870817cb9d4e858190943e2f
Marko Stevanovic <stevanovic.marko@gmail.com>
2021-08-26 18:51:34 +0200

    Added array builtin functions.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,14 +6,25 @@ import parser.Parser
 
 fun main() {
     val input = """
-        let a = [1, 2 * 2, 10 - 5, 8 / 2];
-        a[5];
-        """
+    let map = fn(arr, f) {
+        let iter = fn(arr, accumulated) {
+            if (len(arr) == 0) {
+                accumulated
+            } else {
+                iter(rest(arr), push(accumulated, f(first(arr))));
+            }
+        };
+        iter(arr, []);
+    }
+    let a = [1, 2, 3, 4];
+    let double = fn(x) { x * 2 };
+    map(a, double);
+    """
     val lexer = Lexer(input)
     val parser = Parser(lexer)
     val program = parser.parseProgram()
     println(program)
-    println(parser.errors)
+    println("ERRORS: ${parser.errors}")
     // TODO: Remove this global variable
     val  environment = Environment(mutableMapOf(), null)
     println(eval(program, environment))
--- src/main/kotlin/objectrepr/ObjectRepr.kt
+++ src/main/kotlin/objectrepr/ObjectRepr.kt
@@ -1,5 +1,6 @@
 package objectrepr
 
+import parser.ast.ArrayLiteral
 import parser.ast.BlockStatement
 import parser.ast.Identifier
 
@@ -107,7 +108,7 @@ data class ArrayRepr(val elements: List<ObjectRepr?>): ObjectRepr {
 
 }
 
-data class BuiltinRepr(val fn: (Array<out ObjectRepr?>) -> ObjectRepr) : ObjectRepr {
+data class BuiltinRepr(val fn: (Array<out ObjectRepr?>) -> ObjectRepr?) : ObjectRepr {
     override fun objectType(): ObjectType {
         return DataNames.BUILTIN.toString()
     }
@@ -121,13 +122,66 @@ fun len(vararg input: ObjectRepr?): ObjectRepr {
     if (input.size != 1) {
         return ErrorRepr("wrong number of arguments. got ${input.size}, want=1")
     }
-    val param = input[0]
-    if (param is StringRepr) {
-        return IntegerRepr(param.value.length)
+    return when(val param = input[0]) {
+        is StringRepr -> IntegerRepr(param.value.length)
+        is ArrayRepr -> IntegerRepr(param.elements.size)
+        else -> ErrorRepr("argument type ${param?.objectType()} for 'len' function is not supported")
     }
-    return ErrorRepr("argument type ${param?.objectType()} for 'len' function is not supported")
+}
+
+fun first(vararg input: ObjectRepr?): ObjectRepr? {
+    if (input.size != 1) {
+        return ErrorRepr("wrong number of arguments. got ${input.size}, want=1")
+    }
+    return when(val param = input[0]) {
+        is ArrayRepr -> param.elements.first()
+        else -> ErrorRepr("argument type ${param?.objectType()} for 'first' function is not supported")
+    }
+}
+
+fun last(vararg input: ObjectRepr?): ObjectRepr? {
+    if (input.size != 1) {
+        return ErrorRepr("wrong number of arguments. got ${input.size}, want=1")
+    }
+    return when(val param = input[0]) {
+        is ArrayRepr -> param.elements.last()
+        else -> ErrorRepr("argument type ${param?.objectType()} for 'last' function is not supported")
+    }
+}
+
+fun rest(vararg input: ObjectRepr?): ObjectRepr {
+    if (input.size != 1) {
+        return ErrorRepr("wrong number of arguments. got ${input.size}, want=1")
+    }
+    return when(val param = input[0]) {
+        is ArrayRepr -> {
+            val elements = ArrayList(param.elements)
+            elements.removeFirst()
+            return ArrayRepr(elements)
+        }
+        else -> ErrorRepr("argument type ${param?.objectType()} for 'rest' function is not supported")
+    }
+}
+
+fun push(vararg input: ObjectRepr?): ObjectRepr {
+    if (input.size != 2) {
+        return ErrorRepr("wrong number of arguments. got ${input.size}, want=2")
+    }
+    return when(val param = input[0]) {
+        is ArrayRepr -> {
+            val elements = ArrayList(param.elements)
+            val newElement = input[1]
+            elements.add(newElement)
+            return ArrayRepr(elements)
+        }
+        else -> ErrorRepr("argument type ${param?.objectType()} for 'push' function is not supported")
+    }
 }
 
 val builtinFunctions: Map<String, BuiltinRepr> = mapOf(
-    "len" to BuiltinRepr { args -> len(*args) }
+    "len" to BuiltinRepr { args -> len(*args) },
+    "first" to BuiltinRepr { args -> first(*args) },
+    "last" to BuiltinRepr { args -> last(*args) },
+    "rest" to BuiltinRepr { args -> rest(*args) },
+    "push" to BuiltinRepr { args -> push(*args) }
 )
